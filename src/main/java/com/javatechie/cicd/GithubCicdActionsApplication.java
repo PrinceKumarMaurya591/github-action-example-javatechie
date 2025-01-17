59821611618b8fc1f1747041af811b68645ec70e
PrinceKumarMaurya591 <72147360+PrinceKumarMaurya591@users.noreply.github.com>
2025-01-17 23:59:25 +0530

    code

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -11,7 +11,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Welcome to prince !";
+		return "Welcome to prince maurya !";
 	}
 
 //	echo "# github-actions-example" >> README.md
